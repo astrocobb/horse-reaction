ab7163f056877398862c395248873b19976eb0bd
astrocobb <therrera39@cnm.edu>
2026-02-11 18:57:26 -0700

    Initialize project with React Router template configuration, including TypeScript, TailwindCSS, SSR setup, and Dockerfile for containerization.

---
+++ app/layout/root-layout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router'
+import Navbar from '~/components/navbar'
+import { useState } from 'react'
+import Footer from '~/components/footer'
+
+export default function RootLayout () {
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+  const handleMenuClick = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen)
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
