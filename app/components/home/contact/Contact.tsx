9730b3c14ed25e90a3b7c222a3db04c8f677c023
astro <trevorherrera8710@gmail.com>
2026-02-21 08:18:01 -0800

    style(contact): format Contact component code

--- app/components/home/contact/Contact.tsx
+++ app/components/home/contact/Contact.tsx
@@ -19,21 +19,21 @@ export default function Contact() {
         <h3 className="mb-3 text-left text-base-300 sm:mb-5 md:mb-6 lg:mb-7">
           Please enter your information below, and we will get back to you as soon as possible.
         </h3>
-        <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:items-stretch">
-          <ContactForm/>
-          <div className="flex flex-col gap-4 sm:gap-6 md:gap-8 lg:gap-10">
-            <div className="w-full h-72 overflow-hidden border border-base-700 rounded-md shadow-lg lg:h-80">
-              <ClientOnly>
-                <APIProvider apiKey={ import.meta.env.VITE_MAPS_API_KEY } version="beta">
+        <ClientOnly>
+          <APIProvider apiKey={ import.meta.env.VITE_MAPS_API_KEY } version="beta">
+            <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:items-stretch">
+              <ContactForm/>
+              <div className="flex flex-col gap-4 sm:gap-6 md:gap-8 lg:gap-10">
+                <div className="w-full h-72 overflow-hidden border border-base-700 rounded-md shadow-lg lg:h-80">
                   <HuskyMap/>
-                </APIProvider>
-              </ClientOnly>
+                </div>
+                <div className="grow p-5 bg-base-800/50 border border-base-700 rounded-md shadow-lg sm:p-6 md:p-8 lg:p-10">
+                  <HuskyDetails/>
+                </div>
+              </div>
             </div>
-            <div className="grow p-5 bg-base-800/50 border border-base-700 rounded-md shadow-lg sm:p-6 md:p-8 lg:p-10">
-              <HuskyDetails/>
-            </div>
-          </div>
-        </div>
+          </APIProvider>
+        </ClientOnly>
       </div>
     </section>
   )
