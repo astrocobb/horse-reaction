c62ceefb910131b27af9760f702ce7f08e4fc3c3
astro <trevorherrera8710@gmail.com>
2026-02-21 07:44:43 -0800

    refactor(maps): simplify HuskyMap component by removing custom marker
    Remove HuskyMarker subcomponent and associated marker library usage,
    keeping only the basic Map component with default configuration

--- app/components/maps/HuskyMap.tsx
+++ app/components/maps/HuskyMap.tsx
@@ -1,27 +1,6 @@
-import { useEffect } from 'react'
 import { huskyLatLng } from '~/data/husky-data'
-import { Map, useMap, useMapsLibrary } from '@vis.gl/react-google-maps'
+import { Map } from '@vis.gl/react-google-maps'
 
-
-function HuskyMarker() {
-  const map = useMap()
-  const markerLib = useMapsLibrary('marker')
-
-  useEffect(() => {
-    if (!map || !markerLib) return
-
-    const marker = new markerLib.AdvancedMarkerElement({
-      position: huskyLatLng,
-      map,
-    })
-
-    return () => {
-      marker.map = null
-    }
-  }, [ map, markerLib ])
-
-  return null
-}
 
 export default function HuskyMap() {
   return (
@@ -32,9 +11,7 @@ export default function HuskyMap() {
         defaultZoom={ 15 }
         gestureHandling={ 'greedy' }
         colorScheme="DARK"
-      >
-        <HuskyMarker/>
-      </Map>
+      />
     </div>
   )
 }
